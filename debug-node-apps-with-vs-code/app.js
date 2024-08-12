81eb46bbfb1eb135e87f97e485a7149a0fd63b76
greatxrider <daligdig.jephmari@gmail.com>
2024-08-12 16:05:17 +0800

    debugging node apps

--- debug-node-apps-with-vs-code/app.js
+++ debug-node-apps-with-vs-code/app.js
@@ -38,16 +38,16 @@ app.get('/:userString', (request, response) => {
   //Reverse the string back
   const reversed = reverseString(original);
   //Send to view
-  response.render('reverse', {reversed});
+  response.render('reverse', { reversed });
 });
 
 // catch 404 and forward to error handler
-app.use( (req, res, next) => {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use( (err, req, res, next) =>{
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
